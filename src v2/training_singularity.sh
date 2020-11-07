42ed011e3d3332ecadfdff491b7706ab26e1781a
Marco Ferri <m.ferri17@campus.unimib.it>
2020-11-07 17:07:54 +0100

    Add first singularity definition file

---
+++ src v2/training_singularity.sh
@@ -0,0 +1,14 @@
+# singularity exec --nv --bind $HOME/Desktop/hgfs/thesis:/project tethys/test2.sif ./training_singularity.sh 
+
+python3 ./training.py \
+/project/dataset \
+--regression \
+--data_size 1024 \
+--batch_size 64 \
+--epochs 5 \
+--weights_path /project/v1_original_weights.pickle \
+--retrain_from 24 \
+--augmentation \
+--bgs_folder /project/backgrounds \
+--bgs_name bg_test \
+--debug
