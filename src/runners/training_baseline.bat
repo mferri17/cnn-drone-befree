327a372065d0746063d7c500886c4719f0378169
Marco Ferri <m.ferri17@campus.unimib.it>
2020-12-18 13:21:46 +0100

    Minor change

--- src/runners/training_baseline.bat
+++ src/runners/training_baseline.bat
@@ -3,4 +3,5 @@ C:/Users/96mar/Desktop/meeting_dario/data/orig_train_63720/ ^
 --regression ^
 --weights_path ./../../dev-models/_originals/v1_original_weights.pickle ^
 --verbose 2 ^
+--lr_reducer ^
 --debug
